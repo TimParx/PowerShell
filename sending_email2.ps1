88e109b3e013a1e06cad619c62ad186f2e5a5490
BitSymphony <126241915+TimParx@users.noreply.github.com>
2026-02-12 14:39:30 +0100

    Update SMTP to Gmail

--- sending_email2.ps1
+++ sending_email2.ps1
@@ -1,20 +1,17 @@
 # Put up some environment variables for the mail sending later on
-$mailfrom = "donotreply-se@exelaonline.com"
+$mailfrom = "dwg@xbpeurope.com"
 #$mailto = "pldl_pl_teamleaders_nearshoring@exelaonline.com"
-$mailto = "tomas.skoglund@exelaonline.com"
+$mailto = "maciej.smolinski@xbpeurope.com"
 #$mailcc = "sefrosonmanageroperations@exelaonline.com"
-$subject = "Tempfiles in FF_OC_GLO_PROD"
-$body = "Hi!<br><br>Attached files contain a list of temporary files left in account FF_OC_GLO_PROD.<br><br><br>Br, Skogis"
+$subject = "test"
+$body = "Hi!<br><br>test"
 $attachment54 = "E:\Scripts\FF_OC_GLO_PROD.txt"
 #$attachment55 = "E:\Transfer\kolla_5.5_ehi.txt"
-$smtpserver = "10.34.110.200"
+$smtpserver = "smtp.gmail.com"
 
 # Let's wait a while (2 minutes) to make sure all textfiles have completed
 Start-Sleep -Second 120
 
 # Send the csv files to intended recipiants
-Send-MailMessage -From $mailfrom -To $mailto -Cc $mailcc -Subject $subject -Body $body -BodyAsHtml -Attachments $attachment54 -SmtpServer $smtpserver
+Send-MailMessage -From $mailfrom -To $mailto -Subject $subject -Body $body -BodyAsHtml -SmtpServer $smtpserver
 
-# Remove the csv file after it have been sent with mail.
-Remove-Item E:\Scripts\FF_OC_GLO_PROD.txt
-#Remove-Item E:\Transfer\kolla_5.5_ehi.txt
